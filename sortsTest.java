fed855f8d55d843c907ee1a223b4eb9710376c00
Andrew LeDawson <andrew@andrewiam.com>
2018-01-12 11:59:44 -0800

    Fix test

--- sortsTest.java
+++ sortsTest.java
@@ -13,6 +13,7 @@ public class sortsTest
    {
       int[] selection = new int[25];
       int[] merge = new int[25];
+      int[] quick = new int[25];
       int rand = 0;
       Random random = new Random();
       for (int i = 0; i < 25; i++)
@@ -20,18 +21,20 @@ public class sortsTest
       	 rand = (random.nextInt(2*25) + 1);
          selection[i] = rand;
          merge[i] = rand;
+         quick[i] = rand;
          System.out.println(rand);
       }
       sortsTest.checkSorted(merge, 25);
       Sorts.selectionSort(selection, 25);
       Sorts.mergeSort(merge, 25);
-      System.out.println("\nAfter function run");
+      Sorts.quicksort(quick, 25);
+      System.out.println("\nAfter quick run");
       for (int i = 0; i < 25; i++)
       {
          //System.out.println(selection[i]);
-         System.out.println(merge[i]);
+         System.out.println(quick[i]);
       }
-      sortsTest.checkSorted(merge, 25);
+      sortsTest.checkSorted(quick, 25);
    }
 
    private static void checkSorted(int[] arr, int N)
